4b9b0d5443f159cd5867ba4b5e99dc223b78e7a6
Berkay <berkaytrhn@gmail.com>
2024-08-08 22:30:12 +0300

    softmax initial impl.

--- nn/activations.py
+++ nn/activations.py
@@ -14,3 +14,21 @@ class ReLUActivation:
         TBD
         """
         return np.max(0, input_array)
+    
+
+class SoftmaxActivation:
+    def __init__(self) -> None:
+        pass
+    
+    def __call__(
+        self, 
+        input_array: np.ndarray
+    ) -> np.ndarray:
+        """
+        ReLU Activation Function
+        
+        TBD
+        """
+        # max along axis=1(check among columns, horizontal direction) and shape[0] same with inputs
+        exps = np.exp(input_array - np.max(input_array, axis=1, keepdims=True)) 
+        return exps / np.sum(exps, axis=1, keepdims=True)
